d8771670b87e575062fe4d86d6e247060aade7b9
Mor Saranga <mor.saranga@gmail.com>
2021-04-20 00:00:08 +0300

    style footer

--- register.js
+++ register.js
@@ -1,33 +1,45 @@
-			$(document).ready(function(){
-				// jQuery.validator.addMethod("fullname-onlyletters", function(value, element) {
-  				// allow only letters characters as the username part
-  				// return this.optional( element ) || /^[a-zA-Z])$/.test( value );
-				// }, 'Please enter a valid full name.');
+$(document).ready(function(){
+	// jQuery.validator.addMethod("fullname-onlyletters", function(value, element) {
+	// allow only letters characters as the username part
+	// return this.optional( element ) || /^[a-zA-Z])$/.test( value );
+	// }, 'Please enter a valid full name.');
 
-				$("#register-form").vaildate({
-					rules:{
-						username:"required",
-						fullname:"required",
-						password:{
-							required: true,
-							// minlength: 6
-						},
-						email:"required",
-						date:"required"
-					}
-					,messages:{
-						username: "Please enter a username",
-						fullname: "Please enter your full name",
-						password:{
-							required: "Please enter a password",
-							// minlength: "The password must be at least 6 characters"
-						},
-						email: "Please enter you email",
-						date: "Please enter your birth date"
-					},
-					submitHandler:function(form){
-						form.submit();
-					}
-				});
-				
-			});
+	$("#register-form").vaildate({
+		rules:{
+			username:{
+				required: true
+			},
+			fullname:{
+				required: true
+			},
+			password:{
+				required: true,
+				// minlength: 6
+			},
+			email:{
+				required: true
+			},
+			date:{
+				required: true
+			},
+		}
+		,messages:{
+			username:{
+				required: "Please enter a username",
+			}, 
+			fullname:{
+				required: "Please enter your full name",
+			}, 
+			password:{
+				required: "Please enter a password",
+				// minlength: "The password must be at least 6 characters"
+			},
+			email:{
+				required:  "Please enter you email",
+			},
+			date:{
+				required:  "Please enter your birth date"
+			}, 
+		},
+	});
+});
