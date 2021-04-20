5570f32384a6d7077957c3db3c392d5bc087df48
Mor Saranga <mor.saranga@gmail.com>
2021-04-20 13:16:05 +0300

    trying to fix register didnt work

--- scripts/register.js
+++ scripts/register.js
@@ -1,39 +1,49 @@
 $(document).ready(function(){
-	$("#register-form").vaildate({
-		rules:{
-			username:{
+	localStorage.setItem('p', 'p');
+
+	//REGISTER
+	$("#register_form").validate({
+		rules: {
+			username: {
+				required: true,
+				validateUsername: true
+			},
+			password: {
+				required: true,
+				strongPassword: true
+			},
+			fullname: {
+				required: true,
+				lettersonly: true
+			},
+			email: {
+				required: true,
+				email: true
+			},
+			birthdate: {
 				required: true
-			},
-			fullname:{
-				required: true
-			},
-			password:{
-				required: true
-				// minlength: 6
-			},
-			email:{
-				required: true
-			},
-			date:{
-				required: true
-			},
-		}
-		,messages:{
-			username:{
-				required: "Please enter a username"
-			}, 
-			fullname:{
-				required: "Please enter your full name"
-			}, 
-			password:{
-				required: "Please enter a password"
-			},
-			email:{
-				required:  "Please enter you email"
-			},
-			date:{
-				required:  "Please enter your birth date"
-			}, 
+			}
 		},
-	});
+		messages: {
+			username: {
+				required: "Please enter valid username address.",
+				validateUsername: "Username already taken."
+			},
+			password: {
+				required: "Please enter an password",
+				strongPassword: "password MUST contain at least one character and one number."
+			},
+			fullname: {
+				required: "Please enter a name.",
+				lettersonly: "Full name can be only letters."
+			},
+			email: {
+				required: "Please enter an email address....",
+				email: "Please enter a valid email."
+			},
+			birthdate: {
+				required: "Please enter your birth date."
+			}
+		}
+	});	
 });
