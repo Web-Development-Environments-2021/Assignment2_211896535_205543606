31d7cfca0fc3f6efea5e8aa0cfb2a407bd150d7a
benmarga <benmarga@post.bgu.ac.il>
2021-05-25 14:40:08 +0300

    create git ignoe,env,npm,server run

--- project/main.js
+++ project/main.js
@@ -18,6 +18,7 @@ app.use(
   session({
     cookieName: "session", // the cookie key name
     secret: process.env.COOKIE_SECRET, // the encryption key
+    //secret: "benben",
     duration: 24 * 60 * 60 * 1000, // expired after 20 sec
     activeDuration: 1000 * 60 * 5, // if expiresIn < activeDuration,
     cookie: {
