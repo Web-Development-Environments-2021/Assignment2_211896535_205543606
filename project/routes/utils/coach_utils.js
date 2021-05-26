1ec9b0aa5fec633538f5ea1272f8c9fdb1ca3693
benmarga <benmarga@post.bgu.ac.il>
2021-05-26 14:55:55 +0300

    player page, coach page, team page

--- project/routes/utils/coach_utils.js
+++ project/routes/utils/coach_utils.js
@@ -16,10 +16,22 @@ async function getCoachDetailsById(coachId){
       coach_full_name: coach.data.data.fullname,
       coach_team:team_name,
       coach_image: coach.data.data.image_path,
-      player_common_name:coach.data.data.common_name,
+      coach_common_name:coach.data.data.common_name,
       coach_nationality:coach.data.data.nationality,
       coach_birthdate:coach.data.data.birthdate,
     };
   }
-    
+
+async function getCoachIdByTeam(team_id) {
+    const team = await axios.get(`${api_domain}/teams/${team_id}`,{
+        params: {
+            include: "coach",
+            api_token: process.env.api_token,
+        },
+    });
+    let coach = await getCoachDetailsById(team.data.data.coach.data.coach_id)
+    return coach;
+}
+
+exports.getCoachIdByTeam = getCoachIdByTeam;    
 exports.getCoachDetailsById = getCoachDetailsById;
--- project/routes/utils/players_utils.js
+++ project/routes/utils/players_utils.js
@@ -34,9 +34,10 @@ async function getPlayersInfo(players_ids_list) {
 }
 
 function extractRelevantPlayerData(players_info) {
-  return players_info.map((player_info) => {
+  return  players_info.map((player_info) => {
     const { fullname, image_path, position_id } = player_info.data.data;
     const { name } = player_info.data.data.team.data;
+    
     return {
       name: fullname,
       image: image_path,
