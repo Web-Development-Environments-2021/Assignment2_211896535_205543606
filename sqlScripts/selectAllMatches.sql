11f68d22c396e6c22de259b56ec722e8e121f262
mor-sa <mor.saranga@gmail.com>
2021-05-29 14:41:32 +0300

    fix getclosestmatch

---
+++ sqlScripts/selectAllMatches.sql
@@ -0,0 +1,8 @@
+-- Select rows from a Table or View 'Matches' in schema 'dbo'
+SELECT * FROM dbo.Matches
+--INSERT INTO dbo.Matches (match_id, match_date, match_hour, home_team, away_team, referee_id, stadium)
+--VALUES ('29',CONVERT(date,'2021-06-10',23),convert(datetime,'2021-06-10 20:30:00',120), 'rma', 'ath', '123', 'Santiago Bernabeu')
+--Delete rows from table 'TableName'
+--DELETE FROM dbo.Matches
+--WHERE 	match_id = '31'
+GO
