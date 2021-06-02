5efe560c00b710cb600a55c69b4e0e2cefe3478b
benmarga <benmarga@post.bgu.ac.il>
2021-06-02 14:03:53 +0300

    git ignore

---
+++ tests/login.test.js
@@ -0,0 +1,23 @@
+function sum(x,y){
+    return x+y;
+}
+test('adds 1 + 2 to equal 3', () => {
+    expect(sum(1, 2)).toBe(3);
+  });
+
+test('should add referee successfully', () => {
+    //implement
+  });
+
+test('referee_id already exist,', () => {
+    //implement
+  });
+
+test('invalid details', () => {
+    //implement
+  });
+
+test('unauthorized', () => {
+    //implement
+  });
+
