13c47a3796fcda8ba33dae423e5774b9c9f8318f
benmarga <benmarga@post.bgu.ac.il>
2021-06-03 14:20:27 +0300

    git ignore

--- tests/login.test.js
+++ tests/login.test.js
@@ -1,23 +1,13 @@
-function sum(x,y){
-    return x+y;
-}
-test('adds 1 + 2 to equal 3', () => {
-    expect(sum(1, 2)).toBe(3);
-  });
-
-test('should add referee successfully', () => {
+test('successfully login', () => {
     //implement
   });
 
-test('referee_id already exist,', () => {
+test('username not exist,', () => {
     //implement
   });
 
-test('invalid details', () => {
+test('password incorrect', () => {
     //implement
   });
 
-test('unauthorized', () => {
-    //implement
-  });
-
+//acceptance test for the whold usecase!
--- tests/referee.test.js
+++ tests/referee.test.js
@@ -1,10 +1,3 @@
-function sum(x,y){
-    return x+y;
-}
-test('adds 1 + 2 to equal 3', () => {
-    expect(sum(1, 2)).toBe(3);
-  });
-
 test('should add referee successfully', () => {
     //implement
   });
